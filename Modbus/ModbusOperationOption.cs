bdf65c7005051ba05e3ace9c5a0f6e61464d1edc
csorrells42 <clsorrells42@gmail.com>
2026-06-30 19:28:51 -0500

    Remove automation code from podcast app

--- Modbus/ModbusOperationOption.cs
+++
@@ -1,8 +0,0 @@
-namespace VoiceWorkbench.Modbus;
-
-public sealed record ModbusOperationOption(byte FunctionCode, string Label)
-{
-    public bool IsWrite => FunctionCode is 5 or 6 or 15 or 16;
-
-    public override string ToString() => Label;
-}
